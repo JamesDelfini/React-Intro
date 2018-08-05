3e8a61ef1a505b1213eaa06b4207c27c4d05c4e2
Dennis James Matildo <dj.matildo@gmail.com>
2018-08-05 16:12:54 +0800

    Finished Review React Game Tic Tac Toe

--- src/index.js
+++ src/index.js
@@ -86,9 +86,11 @@ class Game extends React.Component {
         const history = this.state.history;
         const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares); 
-        
+
         const moves = history.map((step, move) => {
-            const desc = move ? 'Go to move #' + move : ' Go to game start' 
+            const desc = move ? 
+                    'Go to move # ' + move + ', ' + (move % 2 === 0 ? 'O' : 'X') + '( ' + ', )': 
+                    'Go to game start' 
             return(
                 <li key={move}>
                     <button onClick={() => this.jumpTo(move)}>{desc}</button>
@@ -141,7 +143,7 @@ function calculateWinner(squares){
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
+            return squares[a];
         }
     }
     return null;
